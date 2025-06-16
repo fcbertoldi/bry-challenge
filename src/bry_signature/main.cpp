70d1c56bdaa816cd3789082524c35c49fcfbc5f3
Fernando Costa Bertoldi <fcbertoldi@gmail.com>
2025-06-16 01:25:31 -0300

    Adicionar executável bry-signature.

---
+++ src/bry_signature/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <fstream>
+#include "bry_challenge/core.h"
+
+
+int main(int argc, char* argv[]) {
+
+    int ret = 1;
+
+    if (argc < 5) {
+        std::cerr << "Usage: " << argv[0] << "<p12file> <passphrase_file> <data_file> <outfile>\n";
+        return ret;
+    }
+
+    const char* p12File = argv[1];
+    const char* passphraseFile = argv[2];
+    const char* dataFile = argv[3];
+    const char* outFile = argv[4];
+
+    std::filesystem::path outPath(outFile);
+    if (outPath.extension() != ".p7s") {
+        std::cerr << "Error: output file must have .p7s extension\n";
+        return ret;
+    }
+
+    std::string passphrase;
+    std::ifstream passphraseStream(passphraseFile);
+    if (!passphraseStream.is_open()) {
+        std::cerr << "Error: Unable to open passphrase file\n";
+        return ret;
+    }
+
+    std::getline(passphraseStream, passphrase);
+    passphraseStream.close();
+
+    if (passphrase.empty()) {
+        std::cerr << "Error: Passphrase file is empty\n";
+        return ret;
+    }
+
+    ret = bry_challenge::cmsSign(p12File, passphrase.data(), dataFile, outFile);
+    return ret;
+}
